5f0a84c5f4910c92d15973d9623ba7eb8ae50b8e
Bruno <brunoarouca@gmail.com>
2026-04-18 00:49:18 -0300

    ignora erros TS na build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ["firebase-admin"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [],
   },
